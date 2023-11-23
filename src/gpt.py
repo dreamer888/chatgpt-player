2058b8214605393b1fa312d5ed88751f6f29d9ac
imcoder <2219992847@qq.com>
2023-11-23 23:02:59 +0000

    update src/gpt.py.
    Signed-off-by: imcoder <2219992847@qq.com>

--- src/gpt.py
+++ src/gpt.py
@@ -1,11 +1,32 @@
+import sys
+import os
+from os import path
+
 import tkinter as tk
 from tkinter import filedialog, ttk
+import pyttsx3
 import threading
-import time
-from gtts import gTTS
-import os
-from pygame import mixer
 from PIL import Image, ImageTk
+import cv2
+
+import vlc
+import platform
+
+from openai import OpenAI
+
+from pathlib import Path
+import pygame
+from threading import Thread
+import time 
+
+#pip3 install opencv-python
+#pip3  pyinstaller
+#pyinstaller  gtp.py  生成exe文件   #打包成 exe文件
+
+'''
+Author ：75039960@qq.com,  18665802636
+'''
+
 
 
 class ChatGPTApp:
@@ -15,22 +36,18 @@ class ChatGPTApp:
 
         '''
         setx设置永久用户环境变量
-
-        setx env_name env_value
+        setx OPENAI_API_KEY "sk-xxx"
         setx加上/m参数表示设置的是系统的环境变量
         setx env_name env_value /m
-
-        
+ 
         #openai.api_key = os.getenv("OPENAI_API_KEY")
-
         #openai.api_key = 'xxx'
-
         #self.client = OpenAI(api_key="sk-Ckxxx",)
-
-        '''
+        '''  
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"),)
+        print(self.client.models.list())
+        #self.client = OpenAI(api_key="sk-",)
 	 
-
         # 创建用户输入框
         self.user_input_box =inputTextBox
         
@@ -38,7 +55,6 @@ class ChatGPTApp:
       
         show_InputText(True)
         self.user_input = self.user_input_box.get("1.0", "end-1c").replace("\n"," ")
-
 
         def  thread_function():
             def  thread_function_animate():
@@ -52,13 +68,12 @@ class ChatGPTApp:
         if self.user_input:   
             threading.Thread(target=thread_function, daemon=True).start()      
            
-
     def get_chatgpt_response(self, user_input):
         try:
 
-
             response = self.client.chat.completions.create(
-                model="gpt-3.5-turbo",
+                #model="gpt-3.5-turbo",
+                model="gpt-4-turbo",
                 messages=[
                     #{"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": user_input},
@@ -67,6 +82,7 @@ class ChatGPTApp:
                 ]
                 )
             '''
+            #下面的调用模式已经被放弃
             response = OpenAI.Completion.create(
               engine="text-davinci-003",
               prompt=self.user_input,
@@ -297,7 +313,6 @@ class PicturePlayer:
         self.root.after(5000, self.show_image)  # 每3秒更换一次图片
 
 def play_text_with_pyttsx3(text, voice_id, rate):
-    import pyttsx3
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[voice_id].id)
@@ -305,78 +320,194 @@ def play_text_with_pyttsx3(text, voice_id, rate):
     engine.say(text)
     engine.runAndWait()
 
-def play_audio(file):
-    mixer.init()
-    mixer.music.load(file)
-    mixer.music.play()
-
-def type_writer_effect(widget, text, delay=100, style='Typewriter'):
+def type_writer_effect(widget, text, delay=100):
+    widget.config(state='normal')
     widget.delete('1.0', tk.END)
-    if style == 'Typewriter':
-        for char in text:
-            widget.insert(tk.END, char)
-            widget.update()
-            time.sleep(delay / 1000.0)
-    elif style == 'FadeIn':
-        # 实现淡入效果的代码
-        pass
-    # 可以添加更多的动画风格
+    for char in text:
+        widget.insert(tk.END, char)
+        widget.update()
+        widget.after(delay)
+    widget.config(state='disabled')
 
-def play_text_and_animate(text, voice_id, rate, style):
+
+
+def update_subtitle(animate_canvas,full_text):
+
+    def scroll_text(x):
+        animate_canvas.move("subtitle", -2, 0)
+        x -= 2
+        if x + animate_canvas.bbox(text_id)[2] > -100:  # 增加额外的滚动距离
+            animate_canvas.after(25, scroll_text, x)
+        else:
+            animate_canvas.delete(text_id)  # 删除文本，确保没有残留
+                
+    x = animate_canvas.winfo_width()
+    text_id = animate_canvas.create_text(x, 20, text=full_text, tags="subtitle", font=("Helvetica", 16), fill="white", anchor="w")
+    scroll_text(x)
+
+
+def play_text_and_animate(text, voice_id, rate, delay):
     def thread_function():
         play_text_with_pyttsx3(text, voice_id, rate)
-        type_writer_effect(animate, text, delay=int(300 - animate_speed.get()), style=style)
 
+    show_InputText(True)
     threading.Thread(target=thread_function, daemon=True).start()
+    #type_writer_effect(animate, text, delay)
+    update_subtitle(animate_canvas,text)
+    #threading.Thread(target=update_subtitle, args={animate_canvas,text},daemon=True).start()
 
-def upload_image():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        image = Image.open(file_path)
-        image = image.resize((360, 500), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        label = tk.Label(window, image=photo)
-        label.image = photo
-        label.place(x=0, y=0, relwidth=1, relheight=1)
+
 
 def play_from_file():
     file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
     if file_path:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text = file.read()
-            play_text_and_animate(text, voice_selector.current(), int(voice_speed.get()), animationStyle.get())
+        with open(file_path, "r", encoding="utf-8") as file:
+            full_text = "    ".join(line.strip() for line in file)
+      
+        def thread_function():
+            play_text_with_pyttsx3(full_text, voice_selector.current(), int(voice_speed.get()))
+        def scroll_text(x):
+            animate_canvas.move("subtitle", -2, 0)
+            x -= 2
+            if x + animate_canvas.bbox(text_id)[2] > -100:  # 增加额外的滚动距离
+                animate_canvas.after(25, scroll_text, x)
+            else:
+                animate_canvas.delete(text_id)  # 删除文本，确保没有残留
 
-window = tk.Tk()
-window.title("通过CHAT GPT对话生成应用程序")
-window.geometry("360x500")
+        x = animate_canvas.winfo_width()
+        text_id = animate_canvas.create_text(x, 20, text=full_text, tags="subtitle", font=("Helvetica", 16), fill="white", anchor="w")
+        scroll_text(x)
 
-inputText = tk.Text(window, height=3, font=("Arial", 9))
-inputText.grid(row=0, column=0, columnspan=2, sticky='ew')
+        #play_text_and_animate(text, voice_id, rate, delay)
+        #args={full_text, voice_selector.current(), int(voice_speed.get())}
+        #threading.Thread(target=play_text_with_pyttsx3, args=args,daemon=True).start() 
+        threading.Thread(target=thread_function, daemon=True).start() 
+        #args2={animate_canvas, full_text}
+        #threading.Thread(target=update_subtitle, args=args2,daemon=True).start()
+  
+    def play_file_subtitle():
+        with open(self.filename, "r", encoding="utf-8") as file:
+            full_text = "    ".join(line.strip() for line in file)
 
-playFromText = tk.Button(window, text="从编辑框播放", command=lambda: play_text_and_animate(inputText.get("1.0", "end-1c"), voice_selector.current(), int(voice_speed.get()), animationStyle.get()))
-playFromText.grid(row=1, column=0, sticky='ew')
+        x = self.animate_canvas.winfo_width()
+        text_id = self.animate_canvas.create_text(x, 20, text=full_text, tags="subtitle", font=("Helvetica", 16), fill="white", anchor="w")
 
-playFromFile = tk.Button(window, text="从文件播放", command=play_from_file)
-playFromFile.grid(row=1, column=1, sticky='ew')
+        def scroll_text(x):
+            self.animate_canvas.move("subtitle", -2, 0)
+            x -= 2
+            if x + self.animate_canvas.bbox(text_id)[2] > -100:  # 增加额外的滚动距离
+                self.animate_canvas.after(25, scroll_text, x)
+            else:
+                self.animate_canvas.delete(text_id)  # 删除文本，确保没有残留
 
-background = tk.Button(window, text="切换背景", command=upload_image)
-background.grid(row=2, column=0, sticky='ew')
+        scroll_text(x)
 
-animationStyle = ttk.Combobox(window, values=['Typewriter', 'FadeIn'])
-animationStyle.current(0)
-animationStyle.grid(row=2, column=1, sticky='ew')
 
-voice_speed = tk.Scale(window, from_=50, to=200, orient='horizontal')
-voice_speed.grid(row=3, column=0, columnspan=2, sticky='ew')
+def change_background():
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png")])
+    if file_path:
+        image = Image.open(file_path)
+        image = image.resize((360, 600), Image.Resampling.LANCZOS)
+        bg_image = ImageTk.PhotoImage(image)
+        canvas.create_image(0, 0, image=bg_image, anchor='nw')
+        canvas.image = bg_image
 
-animate_speed = tk.Scale(window, from_=0, to=300, orient='horizontal')
-animate_speed.grid(row=4, column=0, columnspan=2, sticky='ew')
 
-animate = tk.Text(window, height=5, font=("Arial", 16))
-animate.grid(row=5, column=0, columnspan=2, sticky='ew')
+def show_InputText(flag=True):
+    if  flag :
+        inputText.grid()
+        picture_player.image_label.grid_remove()
+        videoPlayer.playvideo.grid_remove()
+    else: 
+        inputText.grid_remove()
 
-voice_selector = ttk.Combobox(window, values=["Male", "Female"])
-voice_selector.current(0)
-voice_selector.grid(row=6, column=0, columnspan=2, sticky='ew')
 
-window.mainloop()
+########################main################################################
+
+
+screen_width=400
+screen_height=640
+geometry = "400x640"
+root = tk.Tk()
+root.title("CHAT-GPT对话生成应用程序")
+root.geometry(geometry)
+
+canvas = tk.Canvas(root, width=screen_width, height=screen_height)
+canvas.grid(row=0, column=0, rowspan=11, columnspan=2, sticky="nsew")
+
+
+
+background = tk.Button(root, text="切换风格", command=change_background)
+background.grid(row=1, column=0, columnspan=1,sticky="ew")
+
+
+# 创建图片播放器实例
+picture_player = PicturePlayer(root)
+
+
+# 上传视频按钮
+#upload_video_button = tk.Button(root, text="视频", command=upload_video)
+#upload_video_button.grid(row=1, column=1, sticky="ew")
+ 
+# 创建video播放器实例
+#videoPlayer = VideoPlayer(root)
+
+videoPlayer = VlcVideoPlayer(root)
+
+
+#上传视频按钮
+upload_video_button = tk.Button(root, text="视频", command=videoPlayer.open_video)
+upload_video_button.grid(row=1, column=1, sticky="ew")
+
+
+
+# 添加按钮来选择图片
+add_pictures_button = tk.Button(root, text="图片轮播", command=picture_player.add_images)
+add_pictures_button.grid(row=2, column=0,columnspan=1,sticky="ew")
+
+# 添加按钮来选择图片
+
+
+voice_selector = ttk.Combobox(root, values=["Voice 1", "Voice 2", "Voice 3"])
+voice_selector.grid(row=3, column=0, sticky="ew")
+voice_selector.current(0)  # 设置默认选项为列表中的第一个
+
+animationStyle = ttk.Combobox(root, values=["Style 1", "Style 2", "Style 3"])
+animationStyle.grid(row=3, column=1, sticky="ew")
+animationStyle.current(0)  # 设置默认选项为列表中的第一个
+
+
+playFromText = tk.Button(root, text="从编辑框播放", command=lambda: play_text_and_animate(inputText.get("1.0", "end").replace("\n"," "), voice_selector.current(), int(voice_speed.get()), int(200 - animate_speed.get())))
+playFromText.grid(row=4, column=0, sticky="ew")
+
+playFromFile = tk.Button(root, text="从文件播放", command=play_from_file)
+playFromFile.grid(row=4, column=1, sticky="ew")
+
+
+voice_speed = tk.Scale(root, from_=50, to=200, orient="horizontal", label="语音速度")
+voice_speed.grid(row=5, column=0, columnspan=1, sticky="ew")
+voice_speed.set(148)  # 设置默认 速度为148
+
+animate_speed = tk.Scale(root, from_=50, to=150, orient="horizontal", label="动画速度")
+animate_speed.grid(row=5, column=1, columnspan=1, sticky="ew")
+animate_speed.set(58)  # 设置默认动画速度为58
+
+#animate = tk.Text(root, height=5, state='disabled', wrap='word',bg='black',fg='white',font=('楷体',12,'bold'))
+
+inputText = tk.Text(root, height=6, width=1 ,font=("Arial", 9)) #text="输入文字，然后点击从编辑框播放，就可以转化为语音了~~~")
+inputText.grid(row=6, column=0, columnspan=2, sticky="ew")
+inputText.grid_remove()
+
+
+chatGptApp= ChatGPTApp(root,inputText)
+
+chat_button = tk.Button(root, text="GPT对话",command=chatGptApp.send_message)
+chat_button.grid(row=2, column=1,columnspan=1,sticky="ew")
+
+
+animate_canvas = tk.Canvas(root, bg="black", height=50,width =100)
+animate_canvas.grid(row=7, column=0, columnspan=2, sticky="ew")
+
+
+
+root.mainloop()
